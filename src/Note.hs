fc77610becd7e0b4c90cb92f72473e51bbdf3fbf
ktf <not.ktf@proton.me>
2023-01-05 17:28:42 -0800

    Add some fields to `Metadata` (and fix the name)

--- src/Note.hs
+++ src/Note.hs
@@ -10,14 +10,17 @@ someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 data Note = Note
-  { metadata :: MetaData,
+  { metadata :: Metadata,
     content :: Content
   }
 
-data MetaData = MetaData
+data Metadata = Metadata
   { createdTime :: Int, -- TODO: different type for time
     lastEditedTime :: Int,
-    tags :: [Tag]
+    tags :: [Tag],
+    isTrashed :: Bool,
+    isPinned :: Bool,
+    isArchived :: Bool
   }
 
 type Tag = T.Text
