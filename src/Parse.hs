e634fd9a83b98b3ad9597f5c872d8f40da2389f0
ktf <not.ktf@proton.me>
2023-01-05 20:31:30 -0800

    Initial note-mapping code

--- src/Parse.hs
+++ src/Parse.hs
@@ -5,15 +5,40 @@ import Data.ByteString.Lazy as B
 import Data.Text qualified as T
 import Data.Text.Encoding (encodeUtf8)
 import GHC.Generics (Generic)
-import Note (Note)
+import Note (Metadata (..), Note (..), NoteContent (..))
 
--- TODO!
-parseNote :: T.Text -> Maybe Note
-parseNote = undefined
+type KeepJSON = T.Text
+
+-- Given the JSON for a single Google Keep note, attempt to parse out our
+-- internal Note type.
+parseNote :: KeepJSON -> Maybe Note
+parseNote json = mapNote <$> parseKeepJson json
 
 -- TODO: docs
-parseKeepJson :: T.Text -> Maybe KeepNote
+parseKeepJson :: KeepJSON -> Maybe KeepNote
 parseKeepJson = decode . B.fromStrict . encodeUtf8
+
+mapNote :: KeepNote -> Note
+mapNote n =
+  Note
+    { metadata =
+        Metadata
+          { tags = [], -- TODO
+            lastEditedTime = 0,
+            createdTime = 0,
+            isArchived = False,
+            isPinned = False,
+            isTrashed = False
+          },
+      content = mapNoteContent n
+    }
+
+-- Note that note content must be text OR a checklist, but NOT both.
+mapNoteContent :: KeepNote -> NoteContent
+mapNoteContent n = case (textContent n, listContent n) of
+  (Just textContent, Nothing) -> Text textContent
+  (Nothing, Just listContent) -> Checklist [] -- TODO
+  _ -> error "invalid case" -- TODO: Either
 
 -- An intermediary data type that mirrors the JSON structure for easy parsin'.
 data KeepNote = KeepNote
