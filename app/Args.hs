4fef4a8dcb321ed73548a6f165354bae3c525772
ktf <not.ktf@proton.me>
2023-01-11 20:47:28 -0800

    Stub in some initial CLI options

---
+++ app/Args.hs
@@ -0,0 +1,47 @@
+module Args
+  ( KeptOptions (..),
+    cliOptParser,
+  )
+where
+
+import Options.Applicative
+
+data KeptOptions = KeptOptions
+  { stdIO :: Bool,
+    inFiles :: [FilePath]
+  }
+
+cliOptParser :: ParserInfo KeptOptions
+cliOptParser =
+  info
+    (keptOptionParser <**> helper)
+    ( fullDesc
+        <> header "kept: Extract your data from Google Keep."
+        <> progDesc "TODO: do a thing, give it your JSON files, etc" -- TODO
+    )
+
+keptOptionParser :: Parser KeptOptions
+keptOptionParser =
+  KeptOptions
+    <$> stdIOFlagParser
+    <*> filePathsParser
+
+stdIOFlagParser :: Parser Bool
+stdIOFlagParser =
+  switch
+    ( long "stdio"
+        <> short 's'
+        <> help "..." -- TODO
+    )
+
+-- We get these in from the CLI as a list of paths, but need to transform it
+-- into `InFiles`, the actually-useful type.
+filePathsParser :: Parser [FilePath]
+filePathsParser =
+  some -- `some` == "one or more" (as opposed to `many`, zero or more)
+    ( argument
+        str
+        ( metavar "JSON_FILE(S)"
+            <> help "..." -- TODO
+        )
+    )
--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
 module Main (main) where
 
+import Args (KeptOptions (..), cliOptParser)
 import Kept (exportNoteToStdOut)
-import System.Environment (getArgs)
+import Options.Applicative (execParser)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let filePath = head args -- TODO: temporary, of course, for some quick testing
-  exportNoteToStdOut filePath
+  (KeptOptions stdIO inFiles) <- execParser cliOptParser
+  let filePath = head inFiles -- TODO: temporary, of course, for some quick testing
+  if stdIO
+    then exportNoteToStdOut filePath
+    else error "implement me! ;)"
